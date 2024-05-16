1a6597cf5b34287e5a948e45d1d9988f577a9014
Davron <shermatovdavron6@gmail.com>
2024-05-16 19:51:35 +0500

    javascript var let const

---
+++ 1 dars/index.js
@@ -0,0 +1,6 @@
+// Javascript-Boshida EsmaScript bo'lib chiqib keyin javascriptga o'zgardi.Javascript web sahifa web ilovamizga jonlantirish imkonini beradi tugmalar qo'shishimiz mumkin.
+// Javascript codini 2 xil usulda htmlga ulasak bo'ladi 1)html codi pasiga <script> tegi orqali 2) link orqali
+// Javascript single threaad xisoblanadi Single degani qatorlarni birma bir tartib bilan o'qiydi
+// Javascriptda o'zgaruvchilar  2 xil bo'ladi  o'zgaruvchi va o'zgarmas 
+// O'zgaruvchi let, var yordamida elon qilinadi ikkilasini farqi var elon qilinganda birinchi qatorga o'zgaruvchini elon qiladi let qaysi qatorda elon qilingan bo'lsa shu yerdan boshlab ishlatilishi mumkin
+// O'zgarmaslar const bilan elon qilinadi
